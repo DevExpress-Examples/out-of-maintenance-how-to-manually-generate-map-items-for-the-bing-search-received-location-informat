c27512d1285af28ea444d24b4b1d1e5ef23d54b8
DevExpressExampleBot <fakeEmail>
2021-09-02 20:08:56 +0300

    Source auto update [skip ci]

--- VB/MapControl_ManualSearch/MainWindow.xaml.vb
+++ VB/MapControl_ManualSearch/MainWindow.xaml.vb
@@ -2,35 +2,39 @@
 Imports DevExpress.Xpf.Map
 
 Namespace MapControl_ManualSearch
-    Partial Public Class MainWindow
-        Inherits Window
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+		End Sub
 
-        #Region "#ManualItemGenerationEventHandler"
-        Private Sub BingSearchDataProvider_SearchCompleted(ByVal sender As Object, ByVal e As BingSearchCompletedEventArgs)
-            If e.Cancelled Then
-                Return
-            End If
-            If e.RequestResult.ResultCode <> RequestResultCode.Success Then
-                Return
-            End If
+		#Region "#ManualItemGenerationEventHandler"
+		Private Sub BingSearchDataProvider_SearchCompleted(ByVal sender As Object, ByVal e As BingSearchCompletedEventArgs)
+			If e.Cancelled Then
+				Return
+			End If
+			If e.RequestResult.ResultCode <> RequestResultCode.Success Then
+				Return
+			End If
 
-            storage.Items.Clear()
-            If e.RequestResult.SearchResults.Count <> 0 Then
-                For Each locationInformation As LocationInformation In e.RequestResult.SearchResults
-                    AddMapItem(locationInformation)
-                Next locationInformation
-            Else
-                AddMapItem(e.RequestResult.SearchRegion)
-            End If
-        End Sub
+			storage.Items.Clear()
+			If e.RequestResult.SearchResults.Count <> 0 Then
+				For Each locationInformation As LocationInformation In e.RequestResult.SearchResults
+					AddMapItem(locationInformation)
+				Next locationInformation
+			Else
+				AddMapItem(e.RequestResult.SearchRegion)
+			End If
+		End Sub
 
-        Private Sub AddMapItem(ByVal locationInformation As LocationInformation)
-            storage.Items.Add(New MapDot() With {.Location = locationInformation.Location, .ToolTipPattern = locationInformation.DisplayName, .Size = 20})
-        End Sub
-        #End Region ' #ManualItemGenerationEventHandler
-    End Class
+		Private Sub AddMapItem(ByVal locationInformation As LocationInformation)
+			storage.Items.Add(New MapDot() With {
+				.Location = locationInformation.Location,
+				.ToolTipPattern = locationInformation.DisplayName,
+				.Size = 20
+			})
+		End Sub
+		#End Region ' #ManualItemGenerationEventHandler
+	End Class
 End Namespace
